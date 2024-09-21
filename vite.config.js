bde1484c0e5928ae1ee2252d6e8dadf1a03ca901
Luke Batchelor <batchelor.luke+github@gmail.com>
2024-09-21 15:07:54 +0800

    Fixes manifest for pwa

--- vite.config.js
+++ vite.config.js
@@ -9,28 +9,24 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      includeAssets: ["favicon-32x32.png", "apple-touch-icon.png"],
+      manifestFilename: 'site.webmanifest',
       manifest: {
         name: "Quick Tools",
         short_name: "QuickTools",
         description: "A collection of handy tools",
         theme_color: "#ffffff",
+        start_url: '/',
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
           },
         ],
       },
